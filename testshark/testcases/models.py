ce47bd281387e6d55d478fee45b67679cc1a3f85
Okesi Emmanuel <emmanuelokesi0@gmail.com>
2024-07-02 22:37:19 +0100

    database initiated

--- testshark/testcases/models.py
+++ testshark/testcases/models.py
@@ -1,3 +1,27 @@
+# test_cases/models.py
 from django.db import models
+from projects.models import Project
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+TEST_CASE_STATUS_CHOICES = (
+    ('PASSED', _('Passed')),
+    ('FAILED', _('Failed')),
+    ('SKIPPED', _('Skipped')),
+)
+
+
+class TestCase(models.Model):
+    """Model for a test case."""
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    steps = models.TextField()
+    expected_result = models.TextField(blank=True)
+    status = models.CharField(max_length=10, choices=TEST_CASE_STATUS_CHOICES, default='SKIPPED')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    element_identifiers = models.JSONField(default=dict)
+    programming_language = models.CharField(max_length=20, default='Python')
+
+    def __str__(self):
+        return self.name
